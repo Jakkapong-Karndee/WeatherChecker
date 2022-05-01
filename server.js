4c817878fa3f92024756c82285dc583e0fefe695
Jakkapong Karndee <6222782714@g.siit.tu.ac.th>
2022-05-02 06:14:33 +0700

    authenticate and insert new user from ejs (still got a bug)

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
 var express = require("express")
-/* var bcrypt = require('bcrypt')
-var passport = require('passport')
-var flash = require('express-flash')
-var session = require('express-session')
-var methodOverride = require('method-override')
- */
 const mongoose = require('mongoose');
 const User = require('./models/user');
 var app = express()
+//-----------------------------------authentication part
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const flash = require('express-flash')
+const session = require('express-session')
+const methodOverride = require('method-override')
+
+const initializePassport = require('./passport-config');
+initializePassport(
+    passport,
+    username => User.find(user => user.username === username),
+    id => User.find(user => user.id === id)
+)
+//-------------------------------
 
 //connect to mongodb
 const dbURI = 'mongodb+srv://admin:A395UNE4fFE6mnJ2@des422-mongodb.7jcr1.mongodb.net/DES422-MongoDB?retryWrites=true&w=majority'
-
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then((result) => app.listen(3000))
     .catch((err) => console.log(err));
@@ -20,80 +31,11 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
 app.use(express.static('public'))
 app.use('/css', express.static(__dirname + 'public/css'))
 app.use('/javascript', express.static(__dirname + 'public/javascript'))
-
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
-// insert user
-app.get('/main', (req, res) =>{
-    res.render('index')
-});
-
-app.get('/add-user', (req, res) =>{
-    const user = new User({
-        name:'Jack Sparrow',
-        email:'Jack@gmail.com',
-        username:'jack',
-        password:'1234'
-    });
-
-    user.save()
-    .then((result)=>{
-        res.send(result)
-    })
-    .catch((err)=>{
-        console.log(err);
-    });
-});
-
-// show all users
-app.get('/all-users',(req, res)=>{
-    User.find()
-    .then((result)=>{
-        res.send(result);
-    })
-    .catch((err)=>{
-        conslotchange.log(err);
-    });
-})
-// show user
-app.get('/single-user',(req, res)=>{
-    User.find({username:'jack'})
-    .then((result)=>{
-        res.send(result)
-    })
-    .catch((err)=>{
-        conslotchange.log(err);
-    });
-})
-
-
-app.get('/setting', (req, res)=>{
-    res.redirect('/users');
-});
-
-app.get('/users', (req, res)=>{
-    User.find()
-    .then((result)=>{
-        
-        res.render('setting', {users: result})
-    })
-    .catch((err) =>{
-        console.log(err);
-    })
-})
-
-/* const initializePassport = require('/passport-config')
-initializePassport(
-    passport, 
-    username => users.find(user => user.username === username),
-    id => users.find(user => user.id === id)
-)
-
-const users = [] // < ------------------------------------------------------------------- change to database
-
-
-app.use(express.urlencoded({extended: false}))
+//-----------------------------------authentication part
+app.use(express.urlencoded({ extended: false }))
 app.use(flash())
 app.use(session({
     secret: process.env.SESSION_SECRET,
@@ -104,57 +46,117 @@ app.use(passport.initialize())
 app.use(passport.session())
 app.use(methodOverride('_method'))
 
-app.get("/", checkAuthenticated,(req, res) => {
-    res.render("main.html", { name: req.user.fname});  
+//link to index
+app.get('/', checkAuthenticated, (req, res) => {
+    res.render('index.ejs')
 });
 
-app.get("/", checkNotAuthenticated,(req, res) => {
-    res.render("login.html");  
+//link to login
+app.get('/login', checkNotAuthenticated, (req, res) => {
+    res.render('login.ejs')
 });
 
-app.post("/login", checkNotAuthenticated, passport.authenticate('local', {
-    successRedirect: '/',
-    failureRedirect: '/login',
-    failureFlash: true
-}));
-
-app.get("/signup", checkNotAuthenticated, (req, res) => {
-    res.render("signup.html");  
+//link to signup
+app.get('/signup', checkNotAuthenticated, (req, res) => {
+    res.render('signup.ejs')
 });
 
-app.post("/signup", checkNotAuthenticated, async (req, res) => {
-    try{
-        var hashedPassword = bcrypt.hash(req.body.password, 10)
-        users.push({ //also this one
-            id: Date.now().toString(),
-            fname: req.body.fname,
-            lname: req.body.lname,
+//connect signup with DB & hash password
+app.post('/signup', checkNotAuthenticated, async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        const newuser = new User({
+            name: req.body.name,
             email: req.body.email,
             username: req.body.username,
             password: hashedPassword
-        })
-        res.redirect('/login')
-    }catch{
-        res.redirect('/signup')
+        });
+        newuser.save();
+        res.redirect('/login');
+    } catch {
+        res.redirect('/signup');
     }
 })
 
-app.delete('/logout', (req,res) => {
-    req.logout()
-    res.redirect('/login')
-}) 
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+}))
 
-function checkAuthenticated(req, res, next){
-    if(req.isAuthenticated()){
+app.delete('/logout', (req, res) => {
+    req.logOut()
+    res.redirect('/login')
+})
+
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
         return next()
     }
+
     res.redirect('/login')
 }
 
-function checkNotAuthenticated(req, res, next){
-    if(res.isAuthenticated()){
+function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
         return res.redirect('/')
     }
     next()
 }
-*/
+
+//---------------------------------------------------------
+// insert user
+app.get('/add-user', (req, res) => {
+    const user = new User({
+        name: 'Jack Sparrow',
+        email: 'Jack@gmail.com',
+        username: 'jack',
+        password: '1234'
+    });
+
+    user.save()
+        .then((result) => {
+            res.send(result)
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
+// show all users
+app.get('/all-users', (req, res) => {
+    User.find()
+        .then((result) => {
+            res.send(result);
+        })
+        .catch((err) => {
+            conslotchange.log(err);
+        });
+})
+// show user
+app.get('/single-user', (req, res) => {
+    User.find({ username: 'jack' })
+        .then((result) => {
+            res.send(result)
+        })
+        .catch((err) => {
+            conslotchange.log(err);
+        });
+})
+
+
+app.get('/setting', (req, res) => {
+    res.redirect('/users');
+});
+
+app.get('/users', (req, res) => {
+    User.find()
+        .then((result) => {
+
+            res.render('setting', { users: result })
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+})
+
